7ce1190dcfb5a4cd1b3d1003af2a25332dacc096
Tyler Everett <tylertechnotrade@gmail.com>
2021-12-01 16:52:08 -0800

    5th commit

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+var quizStatus = true;
+var questionNumber = 0;
+var answerNumber = 0;
+var score = 0;
+var highScore = 50;
+var finalAnswerCheck = 0;
+var checkTimes = 1;
+var viewHighScoreButtonE1 = document.getElementById('view-high-scores');
+var startQuizButtonE1
